d5b0d2ea17110cbde49d1c784058b07206301833
Rupesh Bhandari <82154284+RupeshBhandari@users.noreply.github.com>
2024-10-04 20:36:14 +0545

    Update main.py for min changes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 class Board:
-  def __init__():
-    ...
-
+    BOARD = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+    ]
+	def __init__():
+        ...
+        
+        
+	def clear_board(board: Board):
+        board = BOARD
+        return board
+        
 class Player:
-  def __init__(name: str):
-    self.name = name
+    def __init__(name: str):
+    	self.name = name
+        
+	def add_player():
+    	return None
+        
+p1 = Player('A')
+b1 = Board('1')
