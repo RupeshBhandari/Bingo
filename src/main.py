22732d91376fe9a478992af9ff16e8596171d1fa
Rupesh Bhandari <rpshbhandari@icloud.com>
2024-10-05 11:50:39 +0545

    added numpy to manipulate the board

--- src/main.py
+++ src/main.py
@@ -1,25 +1,38 @@
+import random
+
+import numpy
 class Board:
-    BOARD = [
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]
-    ]
-	def __init__():
-        ...
+    BOARD = numpy.zeros(shape=[5,5], dtype=int)
+    
+    def __init__(self) -> None:
+        self.max_number = 75
+    
+    @property
+    def get_maxno(self) -> int:
+        return self.max_number
+    
+    @get_maxno
+    def set_maxno(self, maximum_number: int) -> None:
+        self.max_number = maximum_number
         
-        
-	def clear_board(board: Board):
-        board = BOARD
-        return board
-        
+    
 class Player:
-    def __init__(name: str):
-    	self.name = name
-        
-	def add_player():
-    	return None
-        
-p1 = Player('A')
-b1 = Board('1')
+    PLAYER_LIST: list[int] = []
+    def __init__(self, player_id:int) -> None:
+        self.PLAYER_LIST.append(player_id)
+
+class Game:
+    def __init__(self, players: int) -> None:
+        self.load_start_text()
+        for i in range(players):
+            i = Player(player_id=i)
+    
+    @staticmethod
+    def load_start_text() -> None:
+        print('Game Started')
+
+if __name__ == '__main__':
+    players:int = int(input('Enter no of players > '))
+    Game(players=players)
+    print(numpy.zeros(shape=[5,5], dtype=int))
+    
